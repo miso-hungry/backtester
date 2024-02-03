bef770aae03a9aae527733fffa16396f70759cd7
Xiaoyi Dou <xiaoyidou@Xiaoyis-MacBook-Pro.local>
2024-02-03 14:51:08 -0500

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+import datetime
+from datetime import datetime as datetime2
+import glob
+import requests
+
+import streamlit as st
+st.set_page_config(layout="wide")
+import numpy as np
+import pandas as pd
+import plotly.express as pltx
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+st.header('hello world')
